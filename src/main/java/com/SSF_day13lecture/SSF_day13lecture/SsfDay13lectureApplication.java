73c9d6002331a71289af75347246fd62a1d54362
RSYR1906 <rsim9977@gmail.com>
2024-11-20 10:50:42 +0800

    first commit

---
+++ src/main/java/com/SSF_day13lecture/SSF_day13lecture/SsfDay13lectureApplication.java
@@ -0,0 +1,13 @@
+package com.SSF_day13lecture.SSF_day13lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfDay13lectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfDay13lectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SSF_day13lecture/SSF_day13lecture/SsfDay13lectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SSF_day13lecture.SSF_day13lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfDay13lectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
